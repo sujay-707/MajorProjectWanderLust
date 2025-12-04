40caebbcc28e2c98926d40d10912d142ce1dfa04
SuJAY M  Mundaragi <150231026+sujay-707@users.noreply.github.com>
2025-12-04 18:48:16 +0530

    Refactor cloudinary storage configuration

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,19 +2,21 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'wanderlust_DEV',
-      allowedformats:["png" ,"jpg" , "jpeg"],
-    },
-  });   
+  cloudinary: cloudinary,
+  params: {
+    folder: 'wanderlust_DEV',
+    allowed_formats: ["jpg", "jpeg", "png"],
+    upload_preset: "wanderlust_preset"
+  },
+});
 
-  module.exports ={
-    cloudinary,storage
-  }
+module.exports = {
+  cloudinary,
+  storage
+};
